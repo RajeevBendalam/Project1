134dde81678e1c1bf7ba936e69b1e7f66cf411fa
RajeevBendalam <145048736+RajeevBendalam@users.noreply.github.com>
2024-12-18 16:19:12 +0530

    Update AskQuestion.jsx

--- src/pages/AskQuestion.jsx
+++ src/pages/AskQuestion.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import OTPAuthentication from "../components/OTPAuthentication";
 import VideoUpload from "../components/VideoUpload";
 
